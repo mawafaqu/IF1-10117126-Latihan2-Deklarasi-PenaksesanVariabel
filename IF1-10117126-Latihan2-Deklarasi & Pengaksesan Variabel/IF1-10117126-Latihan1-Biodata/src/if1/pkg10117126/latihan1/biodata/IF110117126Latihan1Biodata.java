73caec5910951b03fed8584c2f1554130ed3ea7e
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-21 00:36:55 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan2-Deklarasi & Pengaksesan Variabel/IF1-10117126-Latihan1-Biodata/src/if1/pkg10117126/latihan1/biodata/IF110117126Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * 
+ * @author
+ * NAMA : MAWA FAQU ROCHMAN
+ * KELAS : PBO1
+ * NIM : 10117126
+ * Deskripsi Program : Program ini berisi tentang perintah menampilkan value
+ * ke layar
+ *
+ */
+package if1.pkg10117126.latihan1.biodata;
+
+public class IF110117126Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "--------------------");
+        System.out.println("NAMA\t\t: Mawa Faqu Rochman");
+        System.out.println("ALAMAt\t\t: Kramatwatu Serang Banten");
+        System.out.println("TEMPAT LAHIR\t: Serang");
+        System.out.println("TANGGAL LAHIR   : 05 Juni 1999");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: Nonton Film");
+        System.out.println("MAKES\t\t: Sate");
+
+    }
+
+}
---
+++ IF1-10117126-Latihan2-Deklarasi & Pengaksesan Variabel/src/if1/pkg10117126/latihan2/deklarasi/pengaksesan/variabel/IF110117126Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * 
+ * @author
+ * NAMA : MAWA FAQU ROCHMAN
+ * KELAS : PBO1
+ * NIM : 10117126
+ * Deskripsi Program : Program ini berisi tentang Deklarasi & Pengaksesan
+ * variabel
+ */
+package if1.pkg10117126.latihan2.deklarasi.pengaksesan.variabel;
+
+public class IF110117126Latihan2DeklarasiPengaksesanVariabel {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi variabel 
+
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        //Memberi nilai ke variabel
+
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel nilai = " + PHI);
+        System.out.println("Isi variabel nilai = " + nilaiLogika);
+        System.out.println("Isi variabel nilai = " + nilaiKarakter);
+
+    }
+
+    public IF110117126Latihan2DeklarasiPengaksesanVariabel() {
+
+    }
+
+}
